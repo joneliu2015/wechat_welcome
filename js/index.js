9eef900c2b02cf9a243bec7f01bafba02ec3efb4
joinliu <1028301097@qq.com>
2018-01-03 10:16:48 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+ $(document).ready(function () {
+            $('.link-because').on("click", function () {
+                        $('.submenu-because').slideToggle();
+            });
+            $('.link-step4').on("click", function () {
+                        $('.submenu-step4').slideToggle();;
+            });
+            $('.link-question').on("click", function () {
+                        $('.submenu-question').slideToggle();
+            });
+        });
